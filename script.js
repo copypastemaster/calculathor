8b525ee1a52e1c50dcefaee5495d31edf689b27e
Koki <enoch.binas@gmail.com>
2023-02-23 03:12:57 +0800

    Calculator working.. decimal not added yet

--- script.js
+++ script.js
@@ -31,100 +31,20 @@ const equal = document.querySelector('.equals');
 const clear = document.querySelector('.clear');
 const del = document.querySelector('.undo');
 
- 
-const sum = (a,b) => {
-    return a+b
-};
-
-const prod = (a,b) => {
-    return a * b;
-}
-
-const diff = (a,b) => {
-    return a - b;
-}
-
-const quotient = (a,b) => {
-    return a/b;
-}
-
-const operate = function(pick, firstNum, secondNum){
-    return pick(firstNum, secondNum);
-}
+const arr = [plus, minus, multiply, divide]
 
 
-
-let initValue = null;
-let secondValue = null;
-let total = null;
-
-let toggle = false;
-
-
-function addition () {
-    plus.addEventListener('click', () => {
-        initValue = Number(display.value);
-        display.value = 0;
-        display.value = '';
-        toggle = true;
+arr.forEach((item) => {
+    item.addEventListener('click', () => {
+        display.value += item.value;
     })
+})
 
-    equal.addEventListener('click', () => {
-        secondValue = Number(display.value)
-
-        if (toggle == true) {
-            display.value = initValue + secondValue;
-        }
-        
-        toggle = false;
-    })
-    
-}
-
-plus.addEventListener('click', addition());
-
-
-
-
-// function add () {
-    
-//     console.log(`initval: ${initValue}`)
-
-//     plus.addEventListener('click', () => {
-//         initValue = Number(display.value);
-//         console.log(`secondInitval: ${initValue}`);
-//         display.value = 0;
-//         display.value = '';
-//     })
-    
-//     equal.addEventListener('click', () => {
-//             total = operate(sum, initValue, Number(display.value))
-//             display.value = total;
-//         })
-// }
-// plus.addEventListener('click', add())
-
-
-// function difference () {
-    
-//     console.log(`initval: ${initValue}`)
-
-//     minus.addEventListener('click', () => {
-//         initValue = Number(display.value);
-//         console.log(`secondInitval: ${initValue}`);
-//         display.value = 0;
-//         display.value = '';
-//     })
-    
-//     equal.addEventListener('click', () => {
-//         total = operate(diff, initValue, Number(display.value));
-//         display.value = total;
-//     })
-// }
-// minus.addEventListener('click', difference());
-
-
-
+equal.addEventListener('click', () => {
+    let a = display.value;
+    let b = eval(a);
+    display.value = b;
+})
 
 
 clear.addEventListener('click', () => {
